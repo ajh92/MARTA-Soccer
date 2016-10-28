5cf908a086d1be176e973a2bfd0ede7828190231
ajh92 <andrew@andrewhill.us>
2016-10-28 19:29:40 -0400

    Change to participants

--- app.py
+++ app.py
@@ -4,58 +4,52 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
+PARTICIPANTS = {}
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+def abort_if_participant_doesnt_exist(participant_id):
+    if participant_id not in PARTICIPANTS:
+        abort(404,
+              message="Participant {} doesn't exist".format(participant_id))
 
 parser = reqparse.RequestParser()
 parser.add_argument('task')
 
 
-# Todo
-# shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
+class Participant(Resource):
+    """
+    Shows a single participant and allows deletion of participant
+    """
+    def get(self, participant_id):
+        abort_if_participant_doesnt_exist(participant_id)
+        return PARTICIPANTS[participant_id]
 
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
+    def delete(self, participant_id):
+        abort_if_participant_doesnt_exist(participant_id)
+        del PARTICIPANTS[participant_id]
 
-    def put(self, todo_id):
+    def put(self, participant_id):
         args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
+        participant = {'id': args['participant_id'],
+                       'last_name': args['last_name'],
+                       'given_names': args['given_names']}
+        PARTICIPANTS[participant_id] = participant
 
 
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+class ParticipantList(Resource):
     def get(self):
-        return TODOS
+        return PARTICIPANTS
 
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+    # def post(self):
+    #     args = parser.parse_args()
+    #     todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+    #     todo_id = 'todo%i' % todo_id
+    #     TODOS[todo_id] = {'task': args['task']}
+    #     return TODOS[todo_id], 201
 
-##
-## Actually setup the Api resource routing here
-##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
+
+api.add_resource(ParticipantList, '/api/v1/participants')
+api.add_resource(Participant, '/api/v1/participants/<participants_id>')
 
 
 if __name__ == '__main__':
