26ef635e9dcc7eea1cd9ed68b0020b12cfb4cbfe
ajh92 <andrew@andrewhill.us>
2016-10-29 10:24:38 -0400

    Bug fixes and phone lookup

--- src/CheckInProcessor.py
+++ src/CheckInProcessor.py
@@ -16,7 +16,8 @@ class CheckInProcessor(object):
         s = select([meta.tables['stations']])
         res = connection.execute(s)
         for row in res:
-            self.stations[row['id']] = row['name']
+            self.stations[row['id']] = {'name': row['name'],
+                                        'marta_api_id': row['marta_api_id']}
 
     def get_est_train_time(self, origin_id, dest_id):
         r = (requests.get("http://developer.itsmarta.com/RealtimeTrain/"
@@ -29,16 +30,16 @@ class CheckInProcessor(object):
         arrival_time = '<unknown>'
 
         for arrival in arrivals:
-            if (arrival['STATION'] == self.stations[origin_id] and
-               120 <= int(arrival['WAITING_SECONDS']) <= best_time):
+            if (arrival['STATION'] == self.stations[origin_id]['marta_api_id'] and
+                120 <= int(arrival['WAITING_SECONDS']) <= best_time):
                 current_train = arrival
 
         for arrival in arrivals:
             if(arrival['TRAIN_ID'] == current_train['TRAIN_ID'] and
-               arrival['STATION'] == self.stations[dest_id]):
+               arrival['STATION'] == self.stations[dest_id]['marta_api_id']):
                 arrival_time = arrival['WAITING_TIME']
 
-        return ("Estimated arrival at " + self.stations[dest_id] +
+        return ("Estimated arrival at " + self.stations[dest_id]['name'] +
                 " in " + arrival_time)
 
     def get_est_event_time(self, dest_id):
@@ -54,12 +55,25 @@ class CheckInProcessor(object):
             return self.get_est_event_time(checkin['dest_id'])
 
     def process_checkin(self, checkin):
-        msg = ("Jose checked in at " + self.stations[checkin['origin_id']] +
-               " en route to " + self.stations[checkin['dest_id']] + ". " +
-               self.get_time_update(checkin))
+        s = select([self.meta.tables['individuals']]).where(
+            self.meta.tables['individuals'].c.id == checkin['kid_id'])
+        individual = self.connection.execute(s).fetchone()
 
+        msg = (individual['firstname'] + " checked in at " +
+               self.stations[checkin['origin_id']]['name'] +
+               " en route to " + self.stations[checkin['dest_id']]['name'] +
+               ". " + self.get_time_update(checkin))
+
+        s = select([self.meta.tables['phonenums'].join(
+            self.meta.tables['kids_parents'],
+            self.meta.tables['phonenums'].c.individ_id ==
+            self.meta.tables['kids_parents'].c.parent_id)]).where(
+                self.meta.tables['kids_parents'].c.kid_id == checkin['kid_id'])
+        info = self.connection.execute(s).fetchone()
         print(msg)
+        if info is not None:
+            print(info['phone'])
 #        r = requests.post(os.environ['EASYSMS_URL'] + '/messages',
-  #                        data={'to': '+14044290402', 'body': msg})
+#                        data={'to': '+14044290402', 'body': msg})
 
         return True
